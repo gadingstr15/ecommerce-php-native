36644df92f6ea1ce5431b4fe7f03a076673c319b
Gading Satrio <gadingsatrio546@gmail.com>
2025-08-06 15:28:17 +0700

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+// File: config.example.php
+$host = 'localhost';
+$dbname = 'gasa_store';
+$username = 'ganti_dengan_user_db_anda';
+$password = 'ganti_dengan_password_db_anda';
+
+// ... sisa kode biarkan saja ...
+?>
